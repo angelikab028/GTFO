f53da355df9934188690a41f31ea10fd1fc7ff5f
Angelika Bermudez <aberm028@ucr.edu>
2023-05-25 20:19:49 -0700

    Changing arduino code to implement state machine

--- turret_code/detectionSentry.ino
+++ turret_code/detectionSentry.ino
@@ -1,33 +1,40 @@
 #include <Servo.h>
 
 Servo trigger;
-
+enum State { WAITING, INTRUDER_DETECTED };
 
 void setup() {
-  // start serial port at 9600 bps:
   Serial.begin(9600);
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   while (!Serial) {
-    ; // wait for serial port to connect.
+    ;
   }
   trigger.attach(9);
   trigger.write(0);
-
 }
 
 void loop() {
-  char buffer[16];
-  // if we get a command, turn the LED on or off:
-  if (Serial.available() > 0) {
-    int size = Serial.readBytesUntil('\n', buffer, 12);
-    if (buffer[0] == 'Y') {
-      digitalWrite(LED_BUILTIN, HIGH);
-      trigger.write(180);
-    }
-    if (buffer[0] == 'N') {
-      digitalWrite(LED_BUILTIN, LOW);
-      trigger.write(0);
-    }
+  static char buffer[16];
+  static State state = WAITING;
+  
+  switch (state) {
+    case WAITING:
+      if (Serial.available() > 0) {
+        int size = Serial.readBytesUntil('\n', buffer, 12);
+        state = INTRUDER_DETECTED;
+      }
+      break;
+      
+    case INTRUDER_DETECTED:
+      if (buffer[0] == 'Y') {
+        digitalWrite(LED_BUILTIN, HIGH);
+        trigger.write(180);
+      }
+      else if (buffer[0] == 'N') {
+        digitalWrite(LED_BUILTIN, LOW);
+        trigger.write(0);
+      }
+      state = WAITING;
+      break;
   }
 }
